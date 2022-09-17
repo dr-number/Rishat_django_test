a49ffb92fce109e02da144a3f91cf6e1b7839338
dr.number <dr.number@yandex.ru>
2022-09-17 18:38:54 +0700

    correcting migrations

--- APIStripe/admin.py
+++ APIStripe/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 
-from APIStripe.models import CurrencyItem, Item
-
-class CustomerCurrencyItem(admin.ModelAdmin):
-    list_display = ("currency", )
+from APIStripe.models import Item
 
 class CustomerItem(admin.ModelAdmin):
     list_display = ("name", "description", "price")
 
-
 admin.site.register(Item, CustomerItem)
-admin.site.register(CurrencyItem, CustomerCurrencyItem)
 
 
--- APIStripe/models.py
+++ APIStripe/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-class CurrencyItem(models.Model):
-    currency = models.CharField(max_length=9, default="usd")
-
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
