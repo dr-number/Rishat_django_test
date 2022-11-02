1551bc05fb0ac4cd5e88bfddcd2d787dfe771448
dr.number <dr.number@yandex.ru>
2022-11-02 20:18:16 +0700

    update admin

--- history/admin.py
+++ history/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from history.models import HistoryItem
 
 class CustomerHistoryItem(admin.ModelAdmin):
-    list_display = ("status", "currency", "created_at")
+    list_display = ("user_name", "status", "currency", "created_at")
 
 admin.site.register(HistoryItem, CustomerHistoryItem)
