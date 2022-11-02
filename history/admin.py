d633c71626570f119a4c8530c7e6323ad96f45cf
dr.number <dr.number@yandex.ru>
2022-11-02 20:17:28 +0700

    update admin

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from history.models import HistoryItem
 
-# Register your models here.
+class CustomerHistoryItem(admin.ModelAdmin):
+    list_display = ("status", "currency", "created_at")
+
+admin.site.register(HistoryItem, CustomerHistoryItem)
--- history/models.py
+++ history/models.py
@@ -1,5 +1,5 @@
-from cgitb import text
 from django.db import models
+from django.contrib.auth.models import User
 
 class HistoryItem(models.Model):
     user_id = models.IntegerField()
@@ -7,3 +7,9 @@ class HistoryItem(models.Model):
     currency = models.CharField(max_length=6, default="usd")
     status = models.CharField(max_length=20, default="expectation")
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __get_user(self):
+        return User.objects.get(pk = self.user_id)
+        
+    def user_name(self):
+        return self.__get_user().username
